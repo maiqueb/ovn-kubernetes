f8a2f7d1af0ee5f6612b25e697fb30bd842ebfab
Miguel Duarte Barroso <mdbarroso@redhat.com>
2024-02-22 10:09:12 +0100

    build, k8s.utils.ptr: replace deprecated API
    The "k8s.io/utils/pointer" was deprecated in k8s 1.29 - [0].
    
    This commit applies the suggestion in the upgrade path, by using
    k8s.io/utils.ptr.
    
    [0] - https://github.com/kubernetes/utils/commit/161a940db13fc001fd4148ceaf5291588173eded
    
    Signed-off-by: Miguel Duarte Barroso <mdbarroso@redhat.com>

--- go-controller/cmd/ovnkube-identity/ovnkubeidentity.go
+++ go-controller/cmd/ovnkube-identity/ovnkubeidentity.go
@@ -28,7 +28,7 @@ import (
 	"k8s.io/klog/v2"
 	"k8s.io/kubernetes/pkg/probe"
 	httpprober "k8s.io/kubernetes/pkg/probe/http"
-	utilpointer "k8s.io/utils/pointer"
+	utilpointer "k8s.io/utils/ptr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/certwatcher"
@@ -436,9 +436,9 @@ func runCSRApproverManager(ctx context.Context, leaderID string, restCfg *rest.C
 		LeaderElectionNamespace:       cliCfg.leaseNamespace,
 		LeaderElectionID:              leaderID,
 		LeaderElection:                true,
-		LeaseDuration:                 utilpointer.Duration(time.Minute),
-		RenewDeadline:                 utilpointer.Duration(time.Second * 30),
-		RetryPeriod:                   utilpointer.Duration(time.Second * 20),
+		LeaseDuration:                 utilpointer.To[time.Duration](time.Minute),
+		RenewDeadline:                 utilpointer.To[time.Duration](time.Second * 30),
+		RetryPeriod:                   utilpointer.To[time.Duration](time.Second * 20),
 		LeaderElectionReleaseOnCancel: true,
 	})
 	if err != nil {
@@ -450,8 +450,8 @@ func runCSRApproverManager(ctx context.Context, leaderID string, restCfg *rest.C
 		For(&certificatesv1.CertificateSigningRequest{}, builder.WithPredicates(csrapprover.Predicate)).
 		WithOptions(controller.Options{
 			// Explicitly enable leader election for CSR approver
-			NeedLeaderElection: utilpointer.Bool(true),
-			RecoverPanic:       utilpointer.Bool(true),
+			NeedLeaderElection: utilpointer.To[bool](true),
+			RecoverPanic:       utilpointer.To[bool](true),
 		}).
 		Complete(csrapprover.NewController(
 			mgr.GetClient(),
